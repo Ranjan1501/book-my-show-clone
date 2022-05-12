e34f3f24a8203f7a2f7394e7a07dbd77d448dbd2
Ranjan Kumar Thakur <ranjankumar1757@gmail.com>
2022-05-13 00:30:21 +0530

    removed footer from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
-import Footer from "./components/Footer";
 import Navbar from "./components/navbar.components";
 import { Movies } from "./components/Movies";
 import { Login } from "./components/Login";
